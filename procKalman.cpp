0ad2dca91be6160d64f6a51867e449e4abf8692a
imada kakeru <imada@fun.bio.keio.ac.jp>
2024-07-31 17:47:48 +0900

    relative position

--- procKalman.cpp
+++ procKalman.cpp
@@ -6,11 +6,14 @@ void procKalman(vector<Point> &KLheads, vector<Point> heads){
     float dt = 0.1;
     Kalman::setIdentityMatrix();
     Kalman::setInitialMatrix(dt);
+    Point position(0,0);
     for(int i = 0 ; i < heads.size() ; i++){
         Kalman::setY(heads[i]);
         Kalman::predicting();
         Kalman::updating();
         KLheads.push_back(Kalman::getY());
+        cout << "observed Position = " << heads[i] << endl;
+        cout << "filtered Position = " << Kalman::getY() << endl;
         Kalman::prepare();
     }
     return;
